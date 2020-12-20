594e23cc440f0c153822428e244c8ecf40fe1766
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2020-12-20 15:12:59 +0500

    Complete Lab 10

--- Lab 10/HassaanAli/src/app/app.component.ts
+++ Lab 10/HassaanAli/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Todo } from './todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todos: Todo=[];
   title = 'HassaanAli';
-}
+  editable = true;
+
+  Add(){
+    const mytodolist: Todo = {
+      name: this.name,
+      editable: this.editable
+    }
+
+    this.todos.splice(0,0,mytodolist)
+    
+  }
+
+  Remove(index){
+    this.todos.splice(0,1);
+
+  }
+} 
